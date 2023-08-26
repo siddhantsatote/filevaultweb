e3a08e4bdb092f1c203ca27ac113212f509f42f4
yash <yashvarpe2005@gmail.com>
2023-08-26 19:17:04 +0530

    hero section done with animation

---
+++ src/scripts/script.js
@@ -0,0 +1,11 @@
+// import preloader from "./preloader.js";
+// preloader.load;
+// window.addEventListener("load", function () {
+// 	// Hide the preloader
+// 	const preloader = document.getElementById("preloader");
+// 	preloader.style.display = "none";
+
+// 	// Display the main content
+// 	const mainContent = document.getElementById("main-content");
+// 	mainContent.style.display = "block";
+// });
