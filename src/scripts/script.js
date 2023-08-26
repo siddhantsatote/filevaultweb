40d93dcedf04f07fea48c7df41c6acf2f420bb84
YashVarpe05 <yashvarpe2005@gmail.com>
2023-08-26 19:19:52 +0530

    add the cursor  follower

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,42 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".magnet");
+Shery.hoverWithMediaCircle(".hvr", {
+	videos: ["src/scripts/0.mp4", "src/scripts/3.mp4"],
+});
 // Initialize GSAP and ScrollTrigger
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger); //add the scrolltrigger
+gsap.to(".fleftlm", {
+	scrollTrigger: {
+		trigger: "#fimages",
+		pin: true,
+		start: "top top",
+		end: "bottom bottom",
+		endTrigger: ".last",
+		scrub: 1,
+	},
+	y: "-300%",
+	ease: Power1,
+});
+
+let sections = document.querySelectorAll(".fleftlm");
+
+Shery.imageEffect(".images", {
+	style: 3,
+	config: { onMouse: { value: 1 } },
+	slideStyle: (setScroll) => {
+		sections.forEach(function (section, index) {
+			ScrollTrigger.create({
+				//scroll trigger starting
+				trigger: section,
+				start: "top top",
+				scrub: 1,
+				onUpdate: function (prog) {
+					setScroll(prog.progress + index);
+				},
+			});
+		});
+	},
+});
 
 const backgroundTimeline = gsap.timeline();
 
@@ -11,9 +48,10 @@ backgroundTimeline.to("#featured", {
 
 ScrollTrigger.create({
 	trigger: "#featured",
-	markers: true,
+	// markers: true,
 	start: "-15% top",
 	end: "35% center",
 	animation: backgroundTimeline,
 	scrub: 4,
 });
+// cursor follower
